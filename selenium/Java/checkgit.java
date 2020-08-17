5ba9bbaf9289cdd790eefa8d86c28f9a0e245fea
KIEU NHAT DANG <kieuvantruyen.3dfish@gmail.com>
2020-08-17 21:56:11 +0700

    again

--- selenium/Java/checkgit.java
+++ selenium/Java/checkgit.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,8 +26,8 @@ public class checkgit {
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/libraries/chromedriver");
-		// driver = new ChromeDriver();
-		driver = new FirefoxDriver();
+		 driver = new ChromeDriver();
+//		driver = new FirefoxDriver();
 //		driver.manage().window().maximize();
 //		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 //		explicitWait = new WebDriverWait(driver, 30);
