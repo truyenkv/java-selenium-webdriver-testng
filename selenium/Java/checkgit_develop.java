15bdc568a5b8137e8f590ff4e630adb43e1cd88f
KIEU NHAT DANG <kieuvantruyen.3dfish@gmail.com>
2020-08-18 20:00:06 +0700

    push to develop

---
+++ selenium/Java/checkgit_develop.java
@@ -0,0 +1,42 @@
+package Java;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+
+public class checkgit_develop {
+	WebDriverWait explicitWait;
+	Select select;
+	WebDriver driver;
+
+	@Test
+	public void f() {
+
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/libraries/chromedriver");
+		 driver = new ChromeDriver();
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 30);
+		//nothing
+	}
+
+	@AfterClass
+	public void afterClass() {
+
+	}
+
+}
